a176b549b5ae3a817cb19ef07b08314ed1e30352
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-12-02 15:24:15 +0100

    Implemented json parsing

--- src/grammar/cfg.rs
+++ src/grammar/cfg.rs
@@ -2,8 +2,28 @@
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
 pub struct NonTerminal(String);
 
+impl NonTerminal {
+    pub fn new<S: Into<String>>(s: S) -> Self {
+        Self(s.into())
+    }
+    
+    pub fn id(&self) -> &str {
+        &self.0
+    }
+}
+
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
 pub struct Terminal(String);
+
+impl Terminal {
+    pub fn new<S: Into<String>>(s: S) -> Self {
+        Self(s.into())
+    }
+    
+    pub fn content(&self) -> &str {
+        &self.0
+    }
+}
 
 #[derive(Debug, Clone, Eq, Hash, PartialEq)]
 pub enum Symbol {
@@ -15,6 +35,15 @@ pub enum Symbol {
 pub struct ProductionRule {
     lhs: NonTerminal,
     rhs: Vec<Symbol>,
+}
+
+impl ProductionRule {
+    pub fn new(lhs: NonTerminal, rhs: Vec<Symbol>) -> Self {
+        Self {
+            lhs,
+            rhs,
+        }
+    }
 }
 
 pub struct ContextFreeGrammar {
--- src/parser/json.rs
+++ src/parser/json.rs
@@ -5,18 +5,95 @@ use json_comments::{CommentSettings, StripComments};
 use serde_json as json;
 
 use crate::{
-    ProductionRule,
+    ProductionRule, Symbol, Terminal, NonTerminal,
     error::ParsingError,
 };
+
+fn parse_non_terminal(keyword: &str) -> Option<&str> {
+    if keyword.len() > 2 && keyword.starts_with('<') && keyword.ends_with('>') {
+        Some(&keyword[1..keyword.len() - 1])
+    } else {
+        None
+    }
+}
+
+fn parse_terminal(keyword: &str) -> &str {
+    if keyword.len() >= 2 && keyword.starts_with('\'') && keyword.ends_with('\'') {
+        &keyword[1..keyword.len() - 1]
+    } else {
+        keyword
+    }
+}
+
+fn parse_grammar(value: json::Value) -> Result<Vec<ProductionRule>, String> {
+    let mut rules = Vec::new();
+    
+    let object = match value {
+        json::Value::Object(object) => object,
+        _ => return Err("Peacock grammar must be specified as an object".to_string()),
+    };
+    
+    for (key, value) in &object {
+        // LHS must be a non-terminal
+        let lhs = match parse_non_terminal(key) {
+            Some(lhs) => lhs,
+            None => return Err(format!("'{}' is not a valid non-terminal", key)),
+        };
+        
+        // RHS must be an array of an array of strings that are either terminals or non-terminals
+        let rhs = match value {
+            json::Value::Array(rhs) => rhs,
+            _ => return Err(format!("Right-hand-side of '{}' must be an array", key)),
+        };
+        
+        if rhs.is_empty() {
+            return Err(format!("Invalid production rule '{}': Must not be empty", key));
+        }
+        
+        for rule in rhs {
+            let tokens = match rule {
+                json::Value::Array(tokens) => tokens,
+                _ => return Err(format!("Right-hand-side of '{}' must be an array of arrays", key)),
+            };
+            
+            if tokens.is_empty() {
+                return Err(format!("Invalid production rule '{}': One of its variants is empty", key));
+            }
+            
+            let mut symbols = Vec::new();
+            
+            for token in tokens {
+                let token = match token.as_str() {
+                    Some(token) => token,
+                    _ => return Err(format!("Right-hand-side of '{}' must be an array of arrays of strings", key)),
+                };
+                
+                if let Some(nonterm) = parse_non_terminal(token) {
+                    symbols.push(Symbol::NonTerminal(NonTerminal::new(nonterm)));
+                } else {
+                    let term = parse_terminal(token);
+                    symbols.push(Symbol::Terminal(Terminal::new(term)));
+                }
+            }
+            
+            rules.push(ProductionRule::new(
+                NonTerminal::new(lhs),
+                symbols,
+            ));
+        }
+    }
+    
+    Ok(rules)
+}
 
 pub fn parse_json(path: &Path) -> Result<Vec<ProductionRule>, ParsingError> {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     let reader = StripComments::with_settings(CommentSettings::c_style(), reader);
 
-    let new_part: json::Value = match json::from_reader(reader) {
-        Ok(new_part) => new_part,
-        Err(e) => {
+    let value: json::Value = match json::from_reader(reader) {
+        Ok(value) => value,
+        Err(_) => {
             return Err(ParsingError::new(
                 path,
                 "Invalid JSON syntax"
@@ -24,5 +101,15 @@ pub fn parse_json(path: &Path) -> Result<Vec<ProductionRule>, ParsingError> {
         },
     };
     
-    todo!()
+    parse_grammar(value).map_err(|e| ParsingError::new(path, e))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_peacock() {
+        println!("{:#?}", parse_json(Path::new("test-data/grammars/test-peacock.json")).unwrap());
+    }
 }
