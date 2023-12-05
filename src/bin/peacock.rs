6a544b5374b47ba496d31c3217cad2056752e5e0
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-12-05 02:32:54 +0100

    Enabled multiple cores

--- src/bin/peacock.rs
+++ src/bin/peacock.rs
@@ -7,7 +7,7 @@ use ahash::RandomState;
 use std::time::Duration;
 use nix::sys::signal::Signal;
 use libafl::prelude::{
-    Input, Error, SimpleMonitor, SimpleEventManager,
+    Input, Error, SimpleMonitor,
     HitcountsMapObserver, StdMapObserver,
     TimeObserver, MaxMapFeedback, CalibrationStage, feedback_or,
     TimeFeedback, CrashFeedback, StdState, CachedOnDiskCorpus,
@@ -17,12 +17,12 @@ use libafl::prelude::{
     StdFuzzer, ForkserverExecutor, TimeoutForkserverExecutor,
     Fuzzer, HasTargetBytes, Mutator, MutationResult,
     HasRand, feedback_and, TimeoutFeedback, HasCorpus, Corpus,
-    Generator,
+    Generator, Launcher, EventConfig,
 };
 use libafl_bolts::prelude::{
     UnixShMemProvider, ShMemProvider, ShMem, AsMutSlice,
     current_nanos, StdRand, tuple_list,
-    HasLen, OwnedSlice, Named, Rand,
+    HasLen, OwnedSlice, Named, Rand, Cores,
 };
 use peacock_fuzz::{
     grammar::ContextFreeGrammar,
@@ -32,6 +32,9 @@ use peacock_fuzz::{
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[arg(long, value_name = "CORES")]
+    cores: String,
+    
     #[arg(long, value_name = "GRAMMAR")]
     grammar: String,
     
@@ -200,123 +203,143 @@ impl<S> Generator<PeacockInput, S> for PeacockGenerator {
 
 /* Harness */
 fn fuzz(args: Args) -> Result<(), Error> {
-    let output_dir = Path::new(&args.output);
-    let queue_dir = output_dir.join("queue");
-    let crashes_dir = output_dir.join("crashes");
-    const MAP_SIZE: usize = 2_621_440;
-    let seed = current_nanos();
-    let powerschedule = PowerSchedule::EXPLORE;
-    let timeout = Duration::from_secs(10);
-    let signal = str::parse::<Signal>("SIGKILL").unwrap();
+    let mut run_client = |state: Option<_>, mut mgr, _core_id| {
+        let output_dir = Path::new(&args.output);
+        let queue_dir = output_dir.join("queue");
+        let crashes_dir = output_dir.join("crashes");
+        const MAP_SIZE: usize = 2_621_440;
+        let seed = current_nanos();
+        let powerschedule = PowerSchedule::EXPLORE;
+        let timeout = Duration::from_secs(10);
+        let signal = str::parse::<Signal>("SIGKILL").unwrap();
+        
+        #[cfg(debug_assertions)]
+        let debug_child = true;
+        #[cfg(not(debug_assertions))]
+        let debug_child = false;
+            
+        let mut shmem_provider = UnixShMemProvider::new()?;
+        let mut shmem = shmem_provider.new_shmem(MAP_SIZE)?;
+        shmem.write_to_env("__AFL_SHM_ID")?;
+        let shmem_buf = shmem.as_mut_slice();
+        
+        std::env::set_var("AFL_MAP_SIZE", format!("{}", MAP_SIZE));
+        
+        let edges_observer = unsafe { HitcountsMapObserver::new(StdMapObserver::new("shared_mem", shmem_buf)) };
+        
+        let time_observer = TimeObserver::new("time");
+        
+        let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
+        
+        let calibration = CalibrationStage::new(&map_feedback);
+        
+        let mut feedback = feedback_or!(
+            map_feedback,
+            TimeFeedback::with_observer(&time_observer)
+        );
+        
+        let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
+        let crash_feedback = feedback_and!(
+            CrashFeedback::new(),
+            map_feedback
+        );
+        let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
+        let timeout_feedback = feedback_and!(
+            TimeoutFeedback::new(),
+            map_feedback
+        );
+        let mut objective = feedback_or!(
+            crash_feedback,
+            timeout_feedback
+        );
+        
+        unsafe {
+            grammar_seed.unwrap_unchecked()(seed as usize);
+        };
+        
+        let mut state = if let Some(state) = state {
+            state
+        } else {
+            StdState::new(
+                StdRand::with_seed(seed),
+                CachedOnDiskCorpus::<PeacockInput>::new(&queue_dir, 128)?,
+                OnDiskCorpus::new(&crashes_dir)?,
+                &mut feedback,
+                &mut objective,
+            )?
+        };
+
+        let mutator = PeacockMutator {};
+        
+        let mutational = StdMutationalStage::with_max_iterations(mutator, 0);
+        
+        let scheduler = IndexesLenTimeMinimizerScheduler::new(StdWeightedScheduler::with_schedule(
+            &mut state,
+            &edges_observer,
+            Some(powerschedule),
+        ));
+        
+        let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
+        
+        let forkserver = ForkserverExecutor::builder()
+            .program(&args.cmdline[0])
+            .debug_child(debug_child)
+            .shmem_provider(&mut shmem_provider)
+            .parse_afl_cmdline(args.cmdline.get(1..).unwrap_or(&[]))
+            .coverage_map_size(MAP_SIZE)
+            .is_persistent(false)
+            .build_dynamic_map(edges_observer, tuple_list!(time_observer))?;
+        
+        let mut executor = TimeoutForkserverExecutor::with_signal(forkserver, timeout, signal)?;
+        
+        state.load_initial_inputs(
+            &mut fuzzer,
+            &mut executor,
+            &mut mgr,
+            &[
+                queue_dir,
+                crashes_dir,
+            ]
+        )?;
+        
+        if state.corpus().count() == 0 {
+            let mut generator = PeacockGenerator {};
+            state.generate_initial_inputs(
+                &mut fuzzer,
+                &mut executor,
+                &mut generator,
+                &mut mgr,
+                4096,
+            )?;
+        }
+        
+        let mut stages = tuple_list!(calibration, mutational);
+
+        fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr)?;
+        Ok(())
+    };
     
-    #[cfg(debug_assertions)]
-    let debug_child = true;
-    #[cfg(not(debug_assertions))]
-    let debug_child = false;
+    let shmem_provider = UnixShMemProvider::new()?;
     
     //TODO: tui monitor
     let monitor = SimpleMonitor::new(|s| {
         println!("{s}");
     });
     
-    let mut mgr = SimpleEventManager::new(monitor);
+    let cores = Cores::from_cmdline(&args.cores).expect("Invalid core specification");
     
-    let mut shmem_provider = UnixShMemProvider::new()?;
-    let mut shmem = shmem_provider.new_shmem(MAP_SIZE)?;
-    shmem.write_to_env("__AFL_SHM_ID")?;
-    let shmem_buf = shmem.as_mut_slice();
-    
-    std::env::set_var("AFL_MAP_SIZE", format!("{}", MAP_SIZE));
-    
-    let edges_observer = unsafe { HitcountsMapObserver::new(StdMapObserver::new("shared_mem", shmem_buf)) };
-    
-    let time_observer = TimeObserver::new("time");
-    
-    let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
-    
-    let calibration = CalibrationStage::new(&map_feedback);
-    
-    let mut feedback = feedback_or!(
-        map_feedback,
-        TimeFeedback::with_observer(&time_observer)
-    );
-    
-    let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
-    let crash_feedback = feedback_and!(
-        CrashFeedback::new(),
-        map_feedback
-    );
-    let map_feedback = MaxMapFeedback::tracking(&edges_observer, true, false);
-    let timeout_feedback = feedback_and!(
-        TimeoutFeedback::new(),
-        map_feedback
-    );
-    let mut objective = feedback_or!(
-        crash_feedback,
-        timeout_feedback
-    );
-    
-    unsafe {
-        grammar_seed.unwrap_unchecked()(seed as usize);
-    };
-    
-    let mut state = StdState::new(
-        StdRand::with_seed(seed),
-        CachedOnDiskCorpus::<PeacockInput>::new(&queue_dir, 128)?,
-        OnDiskCorpus::new(&crashes_dir)?,
-        &mut feedback,
-        &mut objective,
-    )?;
-
-    let mutator = PeacockMutator {};
-    
-    let mutational = StdMutationalStage::with_max_iterations(mutator, 0);
-    
-    let scheduler = IndexesLenTimeMinimizerScheduler::new(StdWeightedScheduler::with_schedule(
-        &mut state,
-        &edges_observer,
-        Some(powerschedule),
-    ));
-    
-    let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
-    
-    let forkserver = ForkserverExecutor::builder()
-        .program(&args.cmdline[0])
-        .debug_child(debug_child)
-        .shmem_provider(&mut shmem_provider)
-        .parse_afl_cmdline(args.cmdline.get(1..).unwrap_or(&[]))
-        .coverage_map_size(MAP_SIZE)
-        .is_persistent(false)
-        .build_dynamic_map(edges_observer, tuple_list!(time_observer))?;
-    
-    let mut executor = TimeoutForkserverExecutor::with_signal(forkserver, timeout, signal)?;
-    
-    state.load_initial_inputs(
-        &mut fuzzer,
-        &mut executor,
-        &mut mgr,
-        &[
-            queue_dir,
-            crashes_dir,
-        ]
-    )?;
-    
-    if state.corpus().count() == 0 {
-        let mut generator = PeacockGenerator {};
-        state.generate_initial_inputs(
-            &mut fuzzer,
-            &mut executor,
-            &mut generator,
-            &mut mgr,
-            4096,
-        )?;
+    match Launcher::builder()
+        .shmem_provider(shmem_provider)
+        .configuration(EventConfig::AlwaysUnique)
+        .monitor(monitor)
+        .run_client(&mut run_client)
+        .cores(&cores)
+        .build()
+        .launch()
+    {
+        Err(Error::ShuttingDown) | Ok(()) => Ok(()),
+        e => e,
     }
-    
-    let mut stages = tuple_list!(calibration, mutational);
-
-    fuzzer.fuzz_loop(&mut stages, &mut executor, &mut state, &mut mgr)?;
-    
-    Ok(())
 }
 
 fn main() {
