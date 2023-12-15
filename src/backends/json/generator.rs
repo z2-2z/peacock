6378a5eb33da46ad5e0a75cebd3b1459c688ad8c
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-12-15 13:00:58 +0100

    Added json backend

---
+++ src/backends/json/generator.rs
@@ -0,0 +1,78 @@
+use std::path::Path;
+use serde::ser::Serialize;
+use serde_json::{json, Value, Serializer, ser::PrettyFormatter};
+use std::fs::File;
+use std::io::Write;
+
+use crate::grammar::{ContextFreeGrammar, Symbol};
+
+fn enclosed_in(s: &str, start: char, end: char) -> bool {
+    s.len() >= 2 && s.starts_with(start) && s.ends_with(end)
+}
+
+fn terminal_string(content: &str) -> String {
+    if enclosed_in(content, '<', '>') || enclosed_in(content, '\'', '\'') {
+        return format!("'{}'", content);
+    }
+    
+    content.to_string()
+}
+
+pub struct JsonGenerator {}
+
+impl JsonGenerator {
+    #[allow(clippy::new_without_default)]
+    pub fn new() -> Self {
+        Self {}
+    }
+    
+    pub fn generate<P: AsRef<Path>>(self, path: P, grammar: ContextFreeGrammar) {
+        let mut json = json!({});
+        let object = json.as_object_mut().unwrap();
+        
+        for rule in grammar.rules() {
+            let array = object.entry(format!("<{}>", rule.lhs().id())).or_insert_with(|| Value::Array(Vec::new()));
+            let array = array.as_array_mut().unwrap();
+            
+            let mut insert = Vec::new();
+            for symbol in rule.rhs() {
+                match symbol {
+                    Symbol::Terminal(term) => {
+                        insert.push(Value::String(terminal_string(term.content())));
+                    },
+                    Symbol::NonTerminal(nonterm) => {
+                        insert.push(Value::String(format!("<{}>", nonterm.id())));
+                    },
+                }
+            }
+            
+            array.push(Value::Array(insert));
+        }
+        
+        let mut buf = Vec::new();
+        let formatter = PrettyFormatter::with_indent(b"    ");
+        let mut ser = Serializer::with_formatter(&mut buf, formatter);
+        json.serialize(&mut ser).unwrap();
+        
+        let mut file = File::create(path).expect("Could not open output file");
+        file.write_all(&buf).expect("Could not write to output file");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_generator() {
+        let cfg = ContextFreeGrammar::builder()
+            .gramatron_grammar("test-data/grammars/gramatron.json").unwrap()
+            .optimize(false)
+            .build().unwrap();
+        JsonGenerator::new().generate("/tmp/new.json", cfg);
+        
+        ContextFreeGrammar::builder()
+            .peacock_grammar("/tmp/new.json").unwrap()
+            .build().unwrap();
+    }
+}
