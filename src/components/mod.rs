648df39fcf4c0c2c3c9a38dc9a47deeada50c59b
z2_ <88509734+z2-2z@users.noreply.github.com>
2023-12-17 22:41:20 +0100

    Added checks for loading features

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,9 +1,15 @@
+/* Check the two mutually exclusive features necessary to use the components */
+#[cfg(all(feature = "static-loading", feature = "dynamic-loading"))]
+std::compile_error!("The two features 'static-loading' and 'dynamic-loading' are mutually exclusive");
+
+#[cfg(not(any(feature = "static-loading", feature = "dynamic-loading")))]
+std::compile_error!("One of the features 'static-loading' or 'dynamic-loading' must be activated");
+
 pub(crate) mod ffi;
 mod input;
 mod mutator;
 mod generator;
 
-#[cfg(any(feature = "static-loading", feature = "dynamic-loading"))]
 pub use ffi::load_generator;
 
 pub use ffi::generator_seed as seed_generator;
